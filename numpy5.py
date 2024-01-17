438a43022d255b721c1f94b8276b6ff3d7418557
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2024-01-16 23:30:03 -0300

    Create numpy5.py

---
+++ numpy5.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Gerar dados aleatorios
+
+dados = np.random.randn(1000)
+
+# Calcula a media, desvio padrão e mediana
+mean_value = np.mean(data)
+std_dev = np.std(data)
+median_value = np.median(data)
+
+print("Media: ", mean_value)
+print("Desvio Padrão: ", std_dev)
+print("Mediana", median_value)
